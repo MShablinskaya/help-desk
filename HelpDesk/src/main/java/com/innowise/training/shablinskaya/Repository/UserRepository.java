2737ff84957d15d5112e0e20a07686ff2d4c1fcf
A__Denisenko <motor_Standart@mail.ru>
2021-12-10 23:48:13 +0300

    URGENT! + Каталоги с маленькой буквы пересмотри наименования (не надо писать userId -> id) странный WebAppInit @RestController где dtos? где enums role, state, urgency

--- HelpDesk/src/main/java/com/innowise/training/shablinskaya/Repository/UserRepository.java
+++ HelpDesk/src/main/java/com/innowise/training/shablinskaya/Repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.innowise.training.shablinskaya.Repository;
 import com.innowise.training.shablinskaya.Entity.User;
 
 public interface UserRepository extends TableManagerRepository<User, Integer> {
-    User getByName(String name);
+    User getByName(String name);//зачем тебе user по имени?
 }
